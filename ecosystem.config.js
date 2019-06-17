f3251aa271d7de7d3b2844e2e677d3ddce9287ad
peterchencc <peterchencc@gmail.com>
2019-06-17 23:46:12 +0800

    run `pm2 ecosystem` to generate a sample process deploy config file

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'peterchen.cc',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy : {
+    production : {
+      user : 'deploy',
+      host : '139.162.42.127',
+      ref  : 'origin/master',
+      repo : 'git@github.com:peterchencc/peterchen.cc.git',
+      path : '/home/deploy/peterchen.cc',
+      'post-deploy' : 'npm install && npm run build && npm run serve'
+    }
+  }
+};
