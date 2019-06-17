2fb48ddb59240fadeb2272f5ea4f4e559bc75241
peterchencc <peterchencc@gmail.com>
2019-06-18 01:10:11 +0800

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+	</div>
+)
