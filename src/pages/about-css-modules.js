bf644cc93379e6ff486b6da99499885fa3e2907e
peterchencc <peterchencc@gmail.com>
2019-06-18 23:03:58 +0800

    Build a new page using CSS Modules

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import Container from "../components/container"
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+  </Container>
+)
