854cadb5406d37fc261586ffe9ff672419d75642
peterchencc <peterchencc@gmail.com>
2019-06-18 22:30:04 +0800

    add contact page and import Link from gatsby

---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `lightseagreen` }}>
+    <Link to="/">Home</Link>
+    <p>This is contact page.</p>
+  </div>
+)
