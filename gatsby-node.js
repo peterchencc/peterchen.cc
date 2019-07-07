9f7b1eb64caf30a359d5cf556fa4151134715acf
peterchencc <peterchencc@gmail.com>
2019-07-07 16:43:49 +0800

    create post page from markdown post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -13,8 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = ({ graphql, actions }) => {
-  // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions
   return graphql(`
     {
       allMarkdownRemark {
@@ -29,6 +29,16 @@ exports.createPages = ({ graphql, actions }) => {
     }
   `
   ).then(result => {
-    console.log(JSON.stringify(result, null, 4))
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    })
   })
 }
