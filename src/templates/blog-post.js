9499d7dc47e72a470a31f08706d81605c69079d7
peterchencc <peterchencc@gmail.com>
2019-07-07 17:57:19 +0800

    add React Helmet to post page with siteMetadata

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
   const post = data.markdownRemark
+  const site = data.site
+
   return (
     <Layout>
+      <Helmet>
+        <title>{`${post.frontmatter.title} – ${site.siteMetadata.title}`}</title>
+      </Helmet>
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -19,6 +25,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        title
+      }
+    }
+    site {
+      siteMetadata {
         title
       }
     }
