61283f66e6ea7988f7a73a705daacfc223f10c55
peterchencc <peterchencc@gmail.com>
2019-07-07 13:58:19 +0800

    add gatsby-node.js to createNode and slug

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `
+  ).then(result => {
+    console.log(JSON.stringify(result, null, 4))
+  })
+}
