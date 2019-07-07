69e7f3cf7a2910b16fd557cee2f1c73010b5b9b7
peterchencc <peterchencc@gmail.com>
2019-07-07 17:19:41 +0800

    add manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
