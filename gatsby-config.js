3aee5acb0aface1c0b93c4c3b7842d2319011721
peterchencc <peterchencc@gmail.com>
2021-01-04 22:12:44 +0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,11 +80,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Peter Chen`,
+        short_name: `peterchencc`,
         start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
+        background_color: `#023e8a`,
+        theme_color: `#fff`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
