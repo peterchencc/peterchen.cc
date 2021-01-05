80cc1db62ae6cb91e6fe8192b4a3fc586ba54e72
peterchencc <peterchencc@gmail.com>
2021-01-05 15:32:52 +0800

    hide manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,20 +76,20 @@ module.exports = {
         extensions: [`.md`, `.mdx`],
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Peter Chen`,
-        short_name: `peterchencc`,
-        start_url: `/`,
-        background_color: `#023e8a`,
-        theme_color: `#fff`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        icon: `static/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Peter Chen`,
+    //     short_name: `peterchencc`,
+    //     start_url: `/`,
+    //     background_color: `#023e8a`,
+    //     theme_color: `#fff`,
+    //     // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+    //     // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+    //     display: `standalone`,
+    //     icon: `static/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
