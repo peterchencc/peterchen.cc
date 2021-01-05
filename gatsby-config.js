bd57c758ed1bd60afa611f65f0699d80d3e4fd9a
peterchencc <peterchencc@gmail.com>
2021-01-05 13:18:56 +0800

    change file name

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+        icon: `static/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
